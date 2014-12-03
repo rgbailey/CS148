18fd5f008f6e8fc5a85c6acc4e70175dda499fd3
rgbailey <rgbailey@uvm.edu>
2014-12-03 15:27:28 -0800

    Create info.php

---
+++ info.php
@@ -0,0 +1,46 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>The Northeast Group</title>
+<meta charset="utf-8">
+<meta name="author" content="Romario G. Bailey">
+<meta name="description" content="Assignment10">
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+
+<!--[if lt IE 9]>
+	<script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
+<![endif]-->
+	
+<link rel="stylesheet"
+	  href="mystyle.css"
+      media="screen">
+	<script src="respond.min.js"></script>		
+</head>
+
+<body>
+        <div class="topbar">
+            <div id="topbar-text-left"><a href="https://rgbailey.w3.uvm.edu/cs148/assignment10/index.php">The Northeast Group</a></div>
+            <div id="topbar-text-right"><a href="https://rgbailey.w3.uvm.edu/cs148/assignment10/login.php">Log in</a></div>
+            <div id="topbar-text-right"><a href="https://rgbailey.w3.uvm.edu/cs148/assignment10/register.php">Sign up</a></div>
+            
+        </div>     <article class="fluid infoTime">
+            
+      <section class="fluid first"><p>Increased high school graduation rates</p></section>
+    <section class="fluid second"><p>Lower high school dropout rates</p></section>
+    <section class="fluid third"><p>Healthier relationships and lifestyle choices</p></section>
+    <section class="fluid fourth"><p>Better attitude about school</p></section>
+    <section class="fluid fifth"><p>Higher college enrollment rates and higher educational aspirations</p></section>
+    <section class="fluid second"><p>Enhanced self-esteem and self-confidence</p></section>
+    <section class="fluid third"><p>Improved behavior, both at home and at school</p></section>
+    <section class="fluid fourth"><p>Stronger relationships with parents, teachers, and peers</p></section>
+    <section class="fluid fifth"><p>Improved interpersonal skills</p></section>
+    <section class="fluid second"><p>Decreased likelihood of initiating drug and alcohol use (MENTOR, 2009; Cavell, DuBois, Karcher, Keller, & Rhodes, 2009)</p></section>
+  </article>
+<footer id="footer" class="fluid"><a href="https://rgbailey.w3.uvm.edu/cs148/assignment10/about.php">About</a><br><a href="https://rgbailey.w3.uvm.edu/cs148/assignment10/pics.php">Pictures</a>
+    <hr><p class="fluid foot">This site was designed by Romario G. Bailey<br> © 2014 RGBailey. All rights reserved.</p>
+</footer>
+</body>
+</html>
