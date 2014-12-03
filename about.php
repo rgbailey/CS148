2492f4e8b4f36983a12953f776db6472e3df8a01
rgbailey <rgbailey@uvm.edu>
2014-12-03 15:25:19 -0800

    Create about.php

---
+++ about.php
@@ -0,0 +1,35 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>The Northeast Group</title>
+<meta charset="utf-8">
+<meta name="author" content="Romario G. Bailey">
+<meta name="description" content="Assignment10">
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+
+<!--[if lt IE 9]>
+	<script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
+<![endif]-->
+	
+<link rel="stylesheet"
+	  href="mystyle.css"
+      media="screen">
+	<script src="respond.min.js"></script>		
+</head>
+
+<body>
+        <div class="topbar">
+            <div id="topbar-text-left"><a href="https://rgbailey.w3.uvm.edu/cs148/assignment10/index.php">The Northeast Group</a></div>
+            <div id="topbar-text-right"><a href="https://rgbailey.w3.uvm.edu/cs148/assignment10/login.php">Log in</a></div>
+            <div id="topbar-text-right"><a href="https://rgbailey.w3.uvm.edu/cs148/assignment10/register.php">Sign up</a></div>
+            
+        </div>     <article class="fluid infoTime">
+            
+      <section class="fluid first"><p>This website was made by Romario Bailey for his CS 148 final. I got the idea to make a site about being a mentor 
+          for younger kids when I signed up to be a mentor myself. I has been a really fun experience and I think that many people should try it.</p></section>
+  </article>
+</body>
+</html>
