13f5547a4da4f9187abd834c0acb58f609d2a3b7
rgbailey <rgbailey@uvm.edu>
2014-12-03 15:28:32 -0800

    Create pics.php

---
+++ pics.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>The Northeast Group</title>
+<meta charset="utf-8">
+<meta name="author" content="Romario G. Bailey">
+<meta name="description" content="Assignment10">
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+
+<!--[if lt IE 9]>
+	<script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
+<![endif]-->
+	
+<link rel="stylesheet"
+	  href="mystyle.css"
+      media="screen">
+	<script src="respond.min.js"></script>		
+</head>
+
+<body>
+        <div class="topbar">
+            <div id="topbar-text-left"><a href="https://rgbailey.w3.uvm.edu/cs148/assignment10/index.php">The Northeast Group</a></div>
+            <div id="topbar-text-right"><a href="https://rgbailey.w3.uvm.edu/cs148/assignment10/login.php">Log in</a></div>
+            <div id="topbar-text-right"><a href="https://rgbailey.w3.uvm.edu/cs148/assignment10/register.php">Sign up</a></div>
+            
+        </div>     <article class="fluid infoTime">
+    <a href="javascript:newWindow('img/ph1.jpg','0',430,630)"><img alt="Me and friends" src="img/ph1.jpg"  /></a>
+    <a href="javascript:newWindow('img/ph2.jpg','0',430,630)"><img alt="Me and friends" src="img/ph2.jpg"  /></a>
+    <a href="javascript:newWindow('img/ph3.jpg','0',430,630)"><img alt="Me and friends" src="img/ph3.jpg" /></a>
+
+  </article>
+<footer id="footer" class="fluid"><a href="https://rgbailey.w3.uvm.edu/cs148/assignment10/about.php">About</a><br><a href="https://rgbailey.w3.uvm.edu/cs148/assignment10/pics.php">Pictures</a>
+    <hr><p class="fluid foot">This site was designed by Romario G. Bailey<br> © 2014 RGBailey. All rights reserved.</p>
+</footer>
+</body>
+</html>
